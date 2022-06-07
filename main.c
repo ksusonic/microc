1c54a9afca928a4740ec735571f551eee17a126f
Daniil <ksusonic@yandex.ru>
2022-06-07 16:10:25 +0300

    update

--- main.c
+++ main.c
@@ -1,36 +1,72 @@
-#include <stdio.h>
+#include "stdio.h"
 
-unsigned long int dec_to_binary(int num) {
-    unsigned long int res = 0, d = 1;
-    while (num) {
-        res += (num % 2) * d;
-        num /= 2;
-        d = d * 10;
+unsigned long oct_to_dec(unsigned long number) {
+    unsigned long int num_new = 0, stepen = 1;
+    while (number > 0) {
+        unsigned long int last_dig = (number % 10) * stepen;
+        num_new = num_new + last_dig;
+        number /= 10;
+        stepen *= 8;
+    }
+    number = num_new;
+    return number;
+}
+
+unsigned long int oct_to_binary(unsigned long int number) {
+    number = oct_to_dec(number);
+    unsigned long int res=0, d=1;
+    while(number) {
+        res += (number%2) * d;
+        number /= 2;
+        d *= 10;
     }
     return res;
 }
 
-int oct_to_dec(int number) {
-    int result = 0, stepen = 1;
-    while (number > 0) {
-        result += (number % 10) * stepen;
-        number /= 10;
-        stepen *= 8;
+void dec_to_hex(int number, char code[5]) {
+    number = oct_to_dec(number);
+    for (int i = 4; i >= 0; i--) {
+        int val = (number % 16);
+
+        char c = val <= 9 ? val + '0' : (val - 10) + 'A';
+        code[i] = c;
+        number /= 16;
     }
-    return result;
 }
 
-char *to_ascii(int num, char word[5]) {
-    for (int i = 0; i < 5; i++) {
-        word[i] += num % 10;
-        num /= 10;
-    }
-    return word;
+char get_char(int number) {
+    char res = number + '0';
+    return res;
 }
 
+int get_int(char number) {
+    return number - '0';
+}
+
+void long_to_char(unsigned long int number, char input_string[7]) {
+    int j = 0;
+    int k = 0;
+    char buffer_char[7];
+    if (number == 0) {
+        input_string[0] = '0';
+        input_string[1] = '\0';
+    } else {
+        while (number > 0) {
+            int num = number % 10;
+            buffer_char[j] = get_char(num);
+            number = number / 10;
+            j++;
+        }
+
+        for (k = j - 1; k >= 0; k--)
+            input_string[j - k - 1] = buffer_char[k];
+
+        input_string[j] = '\0';
+    }
+}
 
 int main() {
-    char word[5] = "00000";
-    printf("%lu", dec_to_binary(12345));
-    return 0;
+    char c[5];
+    dec_to_hex(12346, c);
+    printf(c);
 }
